8443a3e3d4783e4b5ffd53629831cb36e7cd155e
iostreamer-X <iostreamer007@gmail.com>
2016-02-09 14:12:49 +0530

    v1.0.0

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 var deasync = require('deasync')
 var exec = deasync(require('child_process').exec)
 function gitcheck() {
-  exec('git fetch')
-  console.log(exec('git status'))
+  exec('git fetch > /dev/null')
+  var status=exec('git status')
+  if(status.indexOf('up-to-date')==-1)
+    console.log(status);
+    exec('notify-send "An update of this app is available."')
 }
 module.exports=gitcheck
